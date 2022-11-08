9f0d1f3ff84614ae333483f600520c64522bccc3
kokok99 <mohdfitri525@gmail.com>
2022-11-08 12:40:39 +0800

    fix rembg

--- simpfy/tools/models.py
+++ simpfy/tools/models.py
@@ -1,6 +1,8 @@
 from django.db import models
+from prof.models import Profile
 
 # Create your models here.
 class Rembg(models.Model):
-    image = models.ImageField(upload_to="RemBg", blank=True, null=True)
-    output = models.ImageField(upload_to="RemBg", blank=True, null=True)
+    user = models.ForeignKey(Profile, on_delete=models.CASCADE, null=True)
+    image = models.ImageField(upload_to="rembg", blank=True, null=True)
+    output = models.ImageField(upload_to="rembg", blank=True, null=True)
--- simpfy/tools/views.py
+++ simpfy/tools/views.py
@@ -2,25 +2,46 @@ from django.shortcuts import render, redirect
 from rembg import remove
 from PIL import Image
 from .models import Rembg
+from prof.models import Profile
+from django.contrib.auth.models import User, auth
 from django.core.files.storage import FileSystemStorage
+import os
+
 
 
 # Create your views here.
 def tools_index(request):
-    return render(request, 'tools/tools_index.html')
+    user_object = User.objects.get(username=request.user.username)
+    user_profile = Profile.objects.get(user=user_object)
+    all_user = Profile.objects.all()
+
+    context = {
+        'user_profile':user_profile,
+        'all_user' : all_user,
+    }
+    return render(request, 'tools/tools_index.html', context)
 
 def tools_rembg(request):
+    user_object = User.objects.get(username=request.user.username)
+    user_profile = Profile.objects.get(user=user_object)
+    all_user = Profile.objects.all()
+    new_pic = Rembg.objects.all().filter(user=user_profile)
     if request.method == "POST":
         image = request.FILES.get('image')
+        im = image.name
         input = Image.open(image)
+        output_path = os.path.splitext(im)[0] + '.png'
         output = remove(input)
-        output.save('media/rembg/output.png')
+        output.save('media/'+output_path)
+        s = Rembg.objects.create(user=user_profile, image=image, output=output_path)
+        s.save()
         return redirect('/tools-rembg')
-    fs = FileSystemStorage()
-    new_pic = fs.generate_filename('media/rembg/output.png')
+    
         
     
     context = {
-        'pics' : new_pic
+        'pics' : new_pic,
+        'user_profile':user_profile,
+        'all_user' : all_user,
     }
     return render(request, 'tools/tools_rembg.html', context)
