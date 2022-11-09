99f865391c1ac88117661c9f82d38b2da48cecd3
kokok99 <mohdfitri525@gmail.com>
2022-11-09 16:07:33 +0800

    deploy

--- simpfy/simpfy/settings.py
+++ simpfy/simpfy/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-d*a4h!!kib=j@+gia@+$ajpgb=cch+)-+o9o$uic2grd-#so(+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
